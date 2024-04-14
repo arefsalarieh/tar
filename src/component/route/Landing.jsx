837eeb7b469661ba5f12229e191159c742b9a7a1
aref <aref@gmail.com>
2024-04-14 11:40:40 +0400

    nw

--- src/component/route/Landing.jsx
+++ src/component/route/Landing.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div className=' border-2 border-orange-800 bg-slate-400 ms-4 me-8 mt-16 mb-96 text-regal-blue'  style={{height:'1000px'}}>
+    <div className=' border-2 border-orange-800 bg-slate-400 w-10/12 ms-4 me-8 mt-16 mb-96 text-regal-blue'  style={{height:'1000px'}}>
       
       Landing
     </div>
